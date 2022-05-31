84c115dcad67ac97cc7faff1b74e64ab3c093bb7
ANKLV <nefesh.samael@gmail.com>
2022-05-31 16:49:30 +0300

    Configure rspec & add spec for user

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { 'TestP@ss' }
+    password_confirmation { 'TestP@ss' }
+  end
+end
