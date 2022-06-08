724da55fe0cf2c4cdf3b5a11be5473a25b20e483
ANKLV <nefesh.samael@gmail.com>
2022-06-09 01:38:48 +0300

    Add devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'home#index'
 end
