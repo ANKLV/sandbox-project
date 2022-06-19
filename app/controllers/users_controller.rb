f6951d249270384ece727be8e7bf54c8b9bac6c1
ANKLV <nefesh.samael@gmail.com>
2022-06-20 01:58:53 +0300

    Deleted extra files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,42 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def show
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.create(user_params)
   end
 
+  def destroy
+    @user.destroy
+  end
+
+  def update
+    new_user_params = user_params.except(:password) if user_params[:password].blank?
+    @user.update(new_user_params || user_params)
+  end
+
+  def edit; end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password, ,:first_name, :last_name, :role)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -145,7 +145,7 @@ ActiveAdmin.setup do |config|
   #
   # Enable and disable Batch Actions
   #
-  # config.batch_actions = true
+  config.batch_actions = false
 
   # == Controller Filters
   #
@@ -306,10 +306,6 @@ ActiveAdmin.setup do |config|
   #    :email,
   #  ]
   # config.filter_method_for_large_association = '_starts_with'
-  ActiveAdmin.register User do
-    preserve_default_filters!
-    remove_filter :reset_password_token, :reset_password_sent_at, :remember_created_at
-  end
 
   # == Head
   #
@@ -339,13 +335,42 @@ ActiveAdmin.setup do |config|
   #
   config.use_webpacker = true
 
-  # ActiveAdmin.register User do
-  #   form do |f|
-  #     f.input :email
-  #     f.input :password
-  #     f.input :role
-  #     f.input :first_name
-  #     f.input :last_name
-  #   end
-  # end
+  ActiveAdmin.register User do
+    preserve_default_filters!
+    remove_filter :reset_password_token, :reset_password_sent_at, :remember_created_at
+
+    index do
+      selectable_column
+      column :id
+      column :email
+      column :role
+      column :first_name
+      column :last_name
+      column :created_at
+      column :updated_at
+      actions
+    end
+
+    show do
+      attributes_table do
+        row :email
+        row :role
+        row :first_name
+        row :last_name
+        row :created_at
+        row :updated_at
+      end
+    end
+
+    form do |f|
+      f.inputs do
+        f.input :email
+        f.input :password
+        f.input :role
+        f.input :first_name
+        f.input :last_name
+      end
+      f.actions
+    end
+  end
 end
