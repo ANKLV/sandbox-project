52b30b8f4865a00bfecfa95f93c448e6dd08b687
ANKLV <nefesh.samael@gmail.com>
2022-06-21 23:50:28 +0300

    Refactor

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -10,6 +10,43 @@ ActiveAdmin.register User do
   #
   # or
   #
+  preserve_default_filters!
+  remove_filter :reset_password_token, :reset_password_sent_at, :remember_created_at
+
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :role
+    column :first_name
+    column :last_name
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :role
+      row :first_name
+      row :last_name
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :encrypted_password, label: "Password"
+      f.input :role
+      f.input :first_name
+      f.input :last_name
+    end
+    f.actions
+  end
+
   permit_params do
     permitted = %i[email encrypted_password role first_name last_name]
     permitted << :other if params[:action] == 'create' && current_user.admin?
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -335,42 +335,4 @@ ActiveAdmin.setup do |config|
   #
   config.use_webpacker = true
 
-  ActiveAdmin.register User do
-    preserve_default_filters!
-    remove_filter :reset_password_token, :reset_password_sent_at, :remember_created_at
-
-    index do
-      selectable_column
-      column :id
-      column :email
-      column :role
-      column :first_name
-      column :last_name
-      column :created_at
-      column :updated_at
-      actions
-    end
-
-    show do
-      attributes_table do
-        row :email
-        row :role
-        row :first_name
-        row :last_name
-        row :created_at
-        row :updated_at
-      end
-    end
-
-    form do |f|
-      f.inputs do
-        f.input :email
-        f.input :encrypted_password, label: "Password"
-        f.input :role
-        f.input :first_name
-        f.input :last_name
-      end
-      f.actions
-    end
-  end
 end
